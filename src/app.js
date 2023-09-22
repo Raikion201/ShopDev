a26537cf4b52f434fc10f1602b344e4379ce4d45
raikion201 <haitrieukt123@gmail.com>
2023-09-22 20:26:58 +0700

    Init

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const compression = require('compression')
+const express = require('express')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const app = express()
+
+
+// init middlewares
+app.use(morgan("common"))
+app.use(helmet())
+app.use(compression())
+// init db
+
+// init routes
+app.get('/' , (req,res,next) => {
+    const strCompress = 'Hello Factipjs'
+
+    return res.status(500).json({
+        message : 'Test',
+        metadata : strCompress.repeat(100000)
+    })
+})
+// handling error
+
+
+
+module.exports = app
